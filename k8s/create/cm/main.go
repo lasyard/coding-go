ec68f0e69d97cd9d5d8ac14d39981230b3f85849
Yueguang Jiao <jiaoyg@zetyun.com>
2025-06-20 18:15:01 +0800

    add k8s

---
+++ k8s/create/cm/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+)
+
+func main() {
+	// Use in-cluster config
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating in-cluster config: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Create clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating clientset: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Build ConfigMap object
+	cm := &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "test",
+		},
+		Data: map[string]string{
+			"name": "Alice",
+			"age":  "18",
+		},
+	}
+
+	// Create the ConfigMap in the "default" namespace
+	_, err = clientset.CoreV1().ConfigMaps("default").Create(context.Background(), cm, metav1.CreateOptions{})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating ConfigMap: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("ConfigMap created successfully!")
+}
